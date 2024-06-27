356eaed1a5e127852f92986e4adac77bed9e48d5
Tido Kubatschek <69509535+kubatschekIDS@users.noreply.github.com>
2024-06-27 13:07:49 +0200

    Update fixedbearing.m
    fix bug

--- +gfx2d/fixedbearing.m
+++ +gfx2d/fixedbearing.m
@@ -204,7 +204,9 @@ classdef fixedbearing < gfx2d.RigidBody
             delete(obj.handl{1});
             delete(obj.handl{2});
             delete(obj.handl{3});
-            obj.window.deleteObject(obj.id);
+            if ~isempty(obj.window)
+                obj.window.deleteObject(obj.id);
+            end
         end
         
         function changeDirection(obj)
